51fb9cc4e735aeb309a7755b3292f97e67209dd1
fguillemet <federico.guillemet@gmail.com>
2018-05-25 11:42:52 -0300

    corrige fecha nacimiento

--- viajesCompartidos/js/isp.js
+++ viajesCompartidos/js/isp.js
@@ -8,13 +8,56 @@ function checkUsuario(theForm) {
 		alert('Ingrese un valor para los datos obligatorios.');
 	} else {
 		if (validarEmail(document.getElementById('correo_electronico').value)){
-			document.getElementById(theForm).submit();
-		}
-		else {
+			if (esfechavalida(document.getElementById('fecha_nacimiento').value)) {
+				document.getElementById(theForm).submit();
+			}
+		} else {
 			alert('Verifique la direccion de correo.');
 		}
 	}
 }
+
+function esfechavalida(fecha) {
+
+    // La longitud de la fecha debe tener exactamente 10 caracteres
+    if ( fecha.length !== 10 )
+       error = true;
+
+    // Primero verifica el patron
+    if ( !/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(fecha) )
+       error = true;
+
+    // Mediante el delimitador "-" separa dia, mes y año
+    var fecha = fecha.split("-");
+    var day = parseInt(fecha[0]);
+    var month = parseInt(fecha[1]);
+    var year = parseInt(fecha[2]);
+
+    // Verifica que dia, mes, año, solo sean numeros
+    error = ( isNaN(day) || isNaN(month) || isNaN(year) );
+
+    // Lista de dias en los meses, por defecto no es año bisiesto
+    var ListofDays = [31,28,31,30,31,30,31,31,30,31,30,31];
+    if ( month === 1 || month > 2 )
+       if ( day > ListofDays[month-1] || day < 0 || ListofDays[month-1] === undefined )
+          error = true;
+
+    // Detecta si es año bisiesto y asigna a febrero 29 dias
+    if ( month === 2 ) {
+       var lyear = ( (!(year % 4) && year % 100) || !(year % 400) );
+       if ( lyear === false && day >= 29 )
+          error = true;
+       if ( lyear === true && day > 29 )
+          error = true;
+    }
+
+    if ( error === true ) {
+       alert("Fecha Inválida: * La Fecha debe tener el formato: dd-mm-aaaa.\n");
+       return false;
+    }
+    else
+    	return true;
+ }
 
 function validarEmail(email ) {
 	if ((email != ' ')&&(email != '')&&(email != null)){
